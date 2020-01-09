fea5fac2ed3574c4ea2545f571fc557fea936d89
Kevin_Isaza <2089276@talnet.nl>
2020-01-09 13:41:43 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,10 +1,12 @@
-alter table planeten modify diameter int not null;
 
-alter table planeten modify `afstand_tot_de_zon` int not null;
 
-alter table planeten modify `massa` int not null;
-
-alter table planeten
-	add bezoek_datum date null;
-
-UPDATE sterrenstelsel.planeten t SET t.bezoek_datum = '2020-01-01' WHERE t.naam LIKE 'Aarde' ESCAPE '#' AND t.diameter = 12756 AND t.`afstand_tot_de_zon` = 149597870 AND t.`massa` = 1 AND t.bezoek_datum IS NULL
+alter table planeten modify `afstand_tot_de_zon` bigint not null;
+alter table planeten modify `massa` float not null;
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Zon', 1392000, 0, 946);
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Mercurius', 4880, 57910000, 0);
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Venus', 12104, 108208930, 1);
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Aarde', 12756, 149597870, 1);
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Mars', 6794, 227936640, 0);
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Jupiter', 142984, 778412010, 318);
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Saturnus', 120536, 1426725400, 95);
+INSERT INTO sterrenstelsel.planeten (naam, diameter, `afstand_tot_de_zon`, `massa`) VALUES ('Uranus', 51118, 2870972200, 15);
